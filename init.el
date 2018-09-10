5d6cbea68aaf47683e14f42eb44de3e72aafb755
grandariel <grands.hawk@gmail.com>
2018-09-11 01:13:35 +0200

    Changes to packages.org and .gitignore, fix init.el

--- init.el
+++ init.el
@@ -8,19 +8,20 @@
 
 (package-initialize)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(org-babel-load-file (expand-file-name "packages.org"))
+(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+;;(org-babel-load-file (expand-file-name "packages.org"))
+(mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("ecba61c2239fbef776a72b65295b88e5534e458dfe3e6d7d9f9cb353448a569e" "4697a2d4afca3f5ed4fdf5f715e36a6cac5c6154e105f3596b44a4874ae52c45" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" default)))
  '(package-selected-packages
    (quote
-    (magit which-key use-package projectile neotree helm general evil doom-themes))))
+    (neotree which-key use-package projectile org-bullets magit helm general evil doom-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
